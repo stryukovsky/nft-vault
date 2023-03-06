b15136ec854bf0cdd54b89030d527eafd30c1252
dmitry <strukovsky1@gmail.com>
2023-03-06 08:45:45 +0400

    test: mint token

--- tests/nft-vault.ts
+++ tests/nft-vault.ts
@@ -2,6 +2,7 @@ import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { NftVault } from "../target/types/nft_vault";
 import { getAssociatedTokenAddress } from "@solana/spl-token";
+import { expect } from "chai";
 
 /**
  * NOTE: run firstly `solana-test-validator`,
@@ -13,6 +14,7 @@ describe("nft-vault", () => {
     const program = anchor.workspace.NftVault as Program<NftVault>;
     const mint = anchor.web3.Keypair.generate();
     const ownerPublicKey = anchor.getProvider().publicKey;
+    const connection = anchor.getProvider().connection;
 
     it("should initialize mint", async () => {
         await program.methods.initializeMint().accounts({
@@ -27,12 +29,18 @@ describe("nft-vault", () => {
             tokenAccount,
         }).rpc();
     });
-    
+
     it("should mint token", async () => {
         const tokenAccount = await getAssociatedTokenAddress(mint.publicKey, ownerPublicKey);
         await program.methods.mintNft(new anchor.BN(123)).accounts({
             mint: mint.publicKey,
             tokenAccount,
         }).rpc();
+    });
+
+    it("should give to interactor nft with defined id", async () => {
+      const tokenAccount = await getAssociatedTokenAddress(mint.publicKey, ownerPublicKey);
+      const balance = await connection.getTokenAccountBalance(tokenAccount)
+      expect(balance.value.amount).eq("123");
     });
 });
